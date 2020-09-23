6cb60b5514f13c7b9766327b66a15b926fcb5df5
Daniel Gonzalez <sonusbeat@hotmail.com>
2020-09-23 12:52:48 -0500

    Header Menu Links

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,25 @@
+const body = document.querySelector('body');
 const hamburguer = document.querySelector('#hamburguer');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const hasFades = document.querySelectorAll('.has-fade');
 
 hamburguer.addEventListener('click', function () {
     if(header.classList.contains('close')) {
         // close hamburguer
         header.classList.remove('close');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        body.classList.remove('close');
+        hasFades.forEach(element => {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     } else {
         // open hamburguer
+        body.classList.add('not-scroll');
         header.classList.add('close');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        hasFades.forEach(element => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
     }
 });
