a86986b479dcc3e7950cdd0c341902ccb5ba01ff
Daniel Gonzalez <sonusbeat@hotmail.com>
2020-09-23 11:48:35 -0500

    Header Overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const hamburguer = document.querySelector('#hamburguer');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+hamburguer.addEventListener('click', function () {
+    if(header.classList.contains('close')) {
+        // close hamburguer
+        header.classList.remove('close');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        // open hamburguer
+        header.classList.add('close');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+});
